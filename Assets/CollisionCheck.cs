4f082b5cefa3df7aa968c2cabe8044fdfd024bd9
Paulo Domingues <dominguespauloco@gmail.com>
2022-06-24 10:00:18 -0300

    Initial commit

---
+++ Assets/CollisionCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+    CapsuleCollider myCollider;
+
+    public Movement playerMovement;
+
+    private void Start()
+    {
+        myCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Physics.IgnoreCollision(myCollider, other);
+        }
+
+        if (other.gameObject.CompareTag("Block"))
+        {
+            playerMovement.CollisionCheck(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Block"))
+        {
+            playerMovement.CollisionCheck(false);
+        }
+    }
+}
